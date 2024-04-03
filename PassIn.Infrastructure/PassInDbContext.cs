736069ca76fd945e88567603d7436629307ce8e6
Marcelo Meirelles <meirellesm@hotmail.com>
2024-04-02 21:04:32 -0300

    Adicionar arquivos de projeto.

---
+++ PassIn.Infrastructure/PassInDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Infrastructure.Entities;
+
+namespace PassIn.Infrastructure;
+
+public class PassInDbContext: DbContext
+{
+    public DbSet<Event> Events { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\Users\\Marcelo\\Desktop\\PROGRAMAÇÃO\\projeto\\nlw-passin-c#\\PassInDb.db");
+    }
+}
